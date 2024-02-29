423a3621dd46fafd333ec908606691e4825abcd5
S.Dhanush <95356096+Dhanush-AI-DS@users.noreply.github.com>
2024-03-01 00:40:22 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,41 +1,3 @@
-# Echoserver
-Echo server and client using python socket
-
-# AIM:
-
-To develop a simple webserver to serve html programming pages.
-
-## DESIGN STEPS:
-
-### Step 1:
-
-Design of echo server and client using python socket
-
-### Step 2:
-
-Implementation using Python code
-
-### Step 3:
-
-Testing the server and client 
-
-## PROGRAM:
-### SERVER SIDE:
-
-```python
-import socket
-HOST = "127.0.0.1"  # The server's hostname or IP address
-PORT = 65432  # The port used by the server
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.sendall(b"Hello, world")
-    data = s.recv(1024)
-print(f"Received {data!r}")
-
-```
-
-### CLIENT SIDE:
-```python
 import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -63,10 +25,3 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             except Exception as e:
                 print(f"Error receiving/sending data: {e}")
                 exit()
-```
-
-## OUTPUT:
-![](server.png)
-![](client.png)
-## RESULT:
-The program is executed successfully
